276d4f24f26a1a93ac98c896039390ffc74f0b40
joanB0ix <joanboixavalos@gmail.com>
2025-01-04 18:14:10 +0100

    Built out skeleton of the homepage with some info

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,59 @@
+import { HeartIcon, SunIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start font-fira-code font-bold">
-        <h1>Hi!!!!</h1>
+    <div className="h-screen flex flex-col font-fira-code w-2/5 m-auto">
+      <nav className="h-20 flex items-center justify-between">
+        <div>
+          <h1 className="font-bold">Joan Boix Àvalos</h1>
+        </div>
+        <div>
+          <SunIcon className="h-5 w-5 mb-1" />
+        </div>
+      </nav>
+      <main className="flex-grow">
+        <div className="my-6">
+          <h2 className="font-semibold pb-2">Who am I?</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, ut
+            aperiam pariatur sapiente dignissimos ratione quia vero explicabo
+            nostrum sed dolorum harum repudiandae fugiat voluptatem est autem,
+            debitis minima cupiditate!
+          </p>
+        </div>
+        <div className="my-6">
+          <h2 className="font-semibold pb-2">My projects</h2>
+          <ul className="list-disc pl-6">
+            <li>
+              <span className="underline">TicTacToe</span>: Built a TicTacToe
+            </li>
+            <li className="underline">Game of Life</li>
+            <li className="underline">Summath</li>
+            <li className="underline">Levenshtein Distance</li>
+          </ul>
+        </div>
+        <div className="my-6">
+          <h2 className="font-semibold pb-2">Socials</h2>
+          <ul className="list-disc pl-6">
+            <li>
+              <Link href="https://github.com/joanB0ix">GitHub</Link>
+            </li>
+            <li>
+              <Link href="https://x.com/JoanBoixAv">X (Formerly twitter)</Link>
+            </li>
+            <li>
+              <Link href="https://www.linkedin.com/in/joanboixavalos/">
+                LinkedIn
+              </Link>
+            </li>
+          </ul>
+        </div>
       </main>
+      <footer className="flex items-center justify-center pb-5">
+        <span className="mr-1">Made with</span>
+        <HeartIcon className="h-5 w-5" />
+      </footer>
     </div>
   );
 }
