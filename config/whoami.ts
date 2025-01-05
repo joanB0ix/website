3a465e0b223c3e344f669454637e2e742194b8a4
joanB0ix <joanboixavalos@gmail.com>
2025-01-05 19:27:24 +0100

    Fix typos i whoami section

--- config/whoami.ts
+++ config/whoami.ts
@@ -1,6 +1,6 @@
 export const WHOAMI =
-  "I&apos;m Joan! I hold a Bachelor&apos;s in Computer Engineering, and\
-        I&apos;m currently studying a Master&apos;s with a focus on distributed\
-        systems. Led and participated on key projects at Amazon, HP, and\
-        Cleverpy, showcasing skills in Java, TypeScript, ReactJS, and AWS among\
-        others. Passionate about technology :)";
+  "I'm Joan! I hold a Bachelor's in Computer Engineering, and\
+    I'm currently studying a Master's with a focus on distributed\
+    systems. Led and participated on key projects at Amazon, HP, and\
+    Cleverpy, showcasing skills in Java, TypeScript, ReactJS, and AWS among\
+    others. Passionate about technology :)";
