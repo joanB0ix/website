d3c941aeb11f0d801e05a018b8971908e605d2b6
joanB0ix <joanboixavalos@gmail.com>
2025-01-05 01:24:21 +0100

    Added page test and coverage check

---
+++ app/__tests__/page.test.tsx
@@ -0,0 +1,17 @@
+import { render } from "@testing-library/react";
+
+import Home from "../page";
+
+jest.mock("../../components/Navbar");
+jest.mock("../about/WhoamiSection");
+jest.mock("../about/SocialsSection");
+jest.mock("../experience/ExperienceSection");
+jest.mock("../projects/ProjectsSection");
+jest.mock("../../components/Footer");
+
+describe("Home component snapshot", () => {
+  it("renders correctly and matches snapshot", () => {
+    const { container } = render(<Home />);
+    expect(container).toMatchSnapshot();
+  });
+});
--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
+/** @type {import('jest').Config} */
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const nextJest = require("next/jest");
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
   dir: './',
@@ -11,6 +12,25 @@ const customJestConfig = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'app/**/*.{js,jsx,ts,tsx}',
+    'components/**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/index.{js,ts}',
+    '!**/*.{test,spec}.{js,jsx,ts,tsx}',
+    '!app/layout.tsx',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 95,
+      branches: 95,
+      functions: 95,
+      lines: 95,
+    },
+  },
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
 };
 
 module.exports = createJestConfig(customJestConfig);
