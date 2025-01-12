96830e500f266aba18b937e63fffbf2b2b0e68d2
joanB0ix <joanboixavalos@gmail.com>
2025-01-12 18:51:45 +0100

    Add sorting_wasm proejct

--- config/projectList.ts
+++ config/projectList.ts
@@ -6,6 +6,12 @@ export interface Project {
 
 export const PROJECT_LIST: Project[] = [
   {
+    title: "sorting_wasm",
+    description:
+      "A WASM library that implements multiple sorting algorithms with snapshot capabilities made in Rust.",
+    url: "https://github.com/joanB0ix/sorting_wasm",
+  },
+  {
     title: "arxiv_wrapper",
     description: "A wrapper of the arXiv API made in Rust.",
     url: "https://github.com/joanB0ix/arxiv_wrapper",
