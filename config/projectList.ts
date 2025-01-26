e1a65d001bfdbfe8f428ff957c7f863d31e77bfd
joanB0ix <joanboixavalos@gmail.com>
2025-01-26 20:47:11 +0100

    Add sorting website project

--- config/projectList.ts
+++ config/projectList.ts
@@ -12,6 +12,12 @@ export const PROJECT_LIST: Project[] = [
     url: "https://github.com/joanB0ix/sorting_wasm",
   },
   {
+    title: "sorting-website",
+    description:
+      "A Sorting Visualizer website that uses sorting_wasm as a library to caluclate the steps.",
+    url: "https://sorting.joanboix.dev",
+  },
+  {
     title: "arxiv_wrapper",
     description: "A wrapper of the arXiv API made in Rust.",
     url: "https://github.com/joanB0ix/arxiv_wrapper",
